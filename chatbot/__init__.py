6c984fa01ad2f9fc0def79531c13a5fa2c2c7f42
CinderKin23 <bonneaij@hudson.k12.wi.us>
2026-04-09 09:46:03 -0500

    Add chatbot module for Grok Roleplay Bot

---
+++ chatbot/__init__.py
@@ -0,0 +1,9 @@
+"""
+Chatbot module for Grok Roleplay Bot
+"""
+
+from .grok_bot import GrokBot
+from .roleplay_bot import RoleplayBot
+from .character import Character
+
+__all__ = ["GrokBot", "RoleplayBot", "Character"]
